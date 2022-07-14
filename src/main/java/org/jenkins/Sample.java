c1d10b6b04faac3a3d4e03afa41a2f4cff924557
sathwik111 <75881035+sathwik111@users.noreply.github.com>
2022-07-14 23:55:18 +0530

    fefef

---
+++ src/main/java/org/jenkins/Sample.java
@@ -0,0 +1,14 @@
+package org.jenkins;
+
+public class Sample {
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello from maven");
+		System.out.println("Hello from Jenkins");
+		
+	}
+	
+	
+	
+}
